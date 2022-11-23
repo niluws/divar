405e3f831f6ddb3cb096b672e6752a25ca562b82
niluws <ns1378@yahoo.com>
2022-11-23 04:51:08 +0330

    serializer and view for adv

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from advertisement.serializers import AdvertisementSerializer
+from advertisement.models import Advertisement
 
-# Create your views here.
+
+class AdvertisementViewSet(ModelViewSet):
+    """ advertisement from Advertisement model """
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['location_id', 'category_id']
+
+    def get_serializer_context(self):
+        return {'request': self.request}
