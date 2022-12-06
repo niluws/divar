7e8874d3bb51cbe325b8eafc5dcf069a33a2a116
niloofar <86254611+niluws@users.noreply.github.com>
2022-12-06 21:54:39 +0330

    Update settings.py

--- divar/settings.py
+++ divar/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-60v9q1lju(92933q9d-9!)rr%d9!p)y*w^v6k*^ji89wwumcat'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rpdivar.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
